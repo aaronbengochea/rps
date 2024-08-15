ccd156c8b967dedde1543afe9ae65aeed41cce88
Aaron Bengochea <71156554+aaronbengochea@users.noreply.github.com>
2024-08-15 12:30:02 -0400

    edits

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const port = 4000
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/visit', (req, res) => {
+    res.json({ message: 'Hello from the backend!' });
+    console.log("ping")
+  });
+  
+  
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
